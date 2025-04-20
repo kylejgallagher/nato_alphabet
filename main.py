fcba4005b7a0747a2257cdcd58f7a08c856ee5e9
Kyle <111585427+kylejgallagher@users.noreply.github.com>
2025-04-20 13:45:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import pandas as pd
 
 nato = pd.read_csv('nato_phonetic_alphabet.csv')
 
-
-
-
 nato_dict = {row.letter: row.code for (index,row) in nato.iterrows()}
-# .
-
 
 word = input("What's the word: ").upper()
 phonetic = [nato_dict[letter] for letter in word]
+
 print(phonetic)
