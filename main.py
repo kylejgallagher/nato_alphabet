8dd21509575f58110cfe89dabcd0a7b8e10cfd18
Kyle <111585427+kylejgallagher@users.noreply.github.com>
2025-04-20 13:44:58 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+nato = pd.read_csv('nato_phonetic_alphabet.csv')
+
+
+
+
+nato_dict = {row.letter: row.code for (index,row) in nato.iterrows()}
+# .
+
+
+word = input("What's the word: ").upper()
+phonetic = [nato_dict[letter] for letter in word]
+print(phonetic)
